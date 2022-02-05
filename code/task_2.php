e4a7585f4e7f240126506476e9856bb68d2a11ba
StrelkovIvan <strelkovik@yandex.ru>
2022-02-05 18:26:09 +0200

    second task

---
+++ code/task_2.php
@@ -0,0 +1,18 @@
+<?php
+
+$integer_1 = 1;
+echo $integer_1;
+
+$integer_2 = 2;
+echo "\n<br>"; 
+echo $integer_2;
+
+$float_1 = 1.1;
+echo "\n<br>",$float_1;
+
+
+echo "\n<br>",11+1;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n<br>",$last_month - $this_month;
